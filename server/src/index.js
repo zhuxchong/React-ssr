aa9aaff59551673a1f6c38916412c769fec24f80
luke <zhuxchong@gmail.com>
2020-04-29 15:43:10 +1000

    initial

---
+++ server/src/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import Home from "./containers/Home";
+import React from "react";
+import { renderToString } from "react-dom/server";
+
+const app = express();
+const content = renderToString(<Home />);
+
+app.get("/", function(req, res) {
+  //res.send(renderToString<Home/>)
+  res.send(`
+		<html>
+			<head>
+				<title>ssr</title>
+			</head>
+			<body>
+				${content}
+			</body>
+		</html>
+  `);
+});
+
+var server = app.listen(3000);
